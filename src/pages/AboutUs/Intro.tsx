793665192a7826c7ed77c78bdbce96b4407e92cd
Vivi J <guodong2404@gmail.com>
2024-10-07 14:17:12 +0800

    remove animation for about page intro section

--- src/pages/AboutUs/Intro.tsx
+++ src/pages/AboutUs/Intro.tsx
@@ -15,19 +15,16 @@ const Intro: React.FC = () => {
           <div className="hidden md:flex bg-gray-300 h-64 items-center justify-center">
             <img src="https://via.placeholder.com/150x84" alt="Placeholder 2" className="w-full h-full rounded-lg object-cover" />
           </div>
-          <ScrollAnimationWrapper>
           <div className="flex flex-col justify-center pt-24 md:pt-0">
             <h2 className="text-3xl font-bold mb-4">{t('aboutUs.intro.title')}</h2>
             <p className="text-lg">
               {t('aboutUs.intro.description')}
             </p>
           </div>
-          </ScrollAnimationWrapper>
-          <ScrollAnimationWrapper>
+ 
           <div className="bg-gray-300 h-64 flex items-center justify-center">
             <img src="https://via.placeholder.com/150x84" alt="Placeholder 3" className="w-full h-full rounded-lg object-cover" />
           </div>
-          </ScrollAnimationWrapper>
         </div>
       </div>
   );
